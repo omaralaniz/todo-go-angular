15e4649b131ac4f4b579ccaf343fc92001ad4246
omaralaniz <omar.alaniz021@gmail.com>
2019-10-10 13:44:50 -0700

    todo app

---
+++ handlers/handler.go
@@ -0,0 +1,64 @@
+package handlers
+
+import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/omaralaniz/golang-angular/todo"
+)
+
+func GetTodoListHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, todo.Get())
+}
+
+func AddTodoHandler(c *gin.Context) {
+	todoItem, statusCode, err := convertHTTPBodyTodo(c.Request.Body)
+	if err != nil {
+		c.JSON(statusCode, err)
+		return
+	}
+	c.JSON(statusCode, gin.H{"id": todo.Add(todoItem.Message)})
+}
+
+func DeleteTodoHandler(c *gin.Context) {
+	todoID := c.Param("id")
+	if err := todo.Delete(todoID); err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
+	c.JSON(http.StatusOK, "")
+}
+
+func CompleteTodoHandler(c *gin.Context) {
+	todoItem, statusCode, err := convertHTTPBodyTodo(c.Request.Body)
+	if err != nil {
+		c.JSON(statusCode, err)
+		return
+	}
+	if todo.Complete(todoItem.ID) != nil {
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
+	c.JSON(http.StatusOK, "")
+}
+
+func convertHTTPBodyTodo(httpBody io.ReadCloser) (todo.Todo, int, error) {
+	body, err := ioutil.ReadAll(httpBody)
+	if err != nil {
+		return todo.Todo{}, http.StatusInternalServerError, err
+	}
+	defer httpBody.Close()
+	return convertJSONBodyToTodo(body)
+}
+
+func convertJSONBodyToTodo(jsonBody []byte) (todo.Todo, int, error) {
+	var todoItem todo.Todo
+	err := json.Unmarshal(jsonBody, &todoItem)
+	if err != nil {
+		return todo.Todo{}, http.StatusBadRequest, err
+	}
+	return todoItem, http.StatusOK, nil
+}
